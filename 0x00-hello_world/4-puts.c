744d0169a6687e1134cb49e3288aa594552e6e6d
Oluwatobi Adeoye <adeoyeoluwatobi90@gmail.com>
2023-03-20 10:03:37 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
 /**
- * main - prints "Programming is like building a multilingual puzzle
- * followed by a new line"
- * return 0.
+ * main - Entry Point
+ *
+ * Return: returns 0 if code executes successfully
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
