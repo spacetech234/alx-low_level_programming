16b410e19a4b2072be23a81f7877dcb187e8b2c3
Oluwatobi Adeoye <adeoyeoluwatobi90@gmail.com>
2023-03-20 09:53:11 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints "Programming is like building a multilingual puzzle followed by a new line"
+ * main - prints "Programming is like building a multilingual puzzle
+ * followed by a new line"
  * return 0.
  */
 
