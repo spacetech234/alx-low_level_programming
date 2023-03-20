1597341db968a94dbfcc0a42b5deae9c39b2c021
Oluwatobi Adeoye <adeoyeoluwatobi90@gmail.com>
2023-03-19 23:01:23 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#!/bin/bash
+#incluce <stdio.h>
+
+/**
+ * main - prints "with proper grammar, but the outcome is
+ * a piece of art," followed newline
+ * Return: Always 0.
+ * 
+ */
+
+int main(void)
+
+{
+	printf("with proper grammar, but the outcome is a piece of art,"\n");
+	return (0)
+}
