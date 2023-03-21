e24bd972cd422ed3afa8aee2ed56e8d6f7e6ad25
Oluwatobi Adeoye <adeoyeoluwatobi90@gmail.com>
2023-03-20 17:53:15 -0700

    100-quote.c

---
+++ 0x00-hello_world/100-quote.c
@@ -0,0 +1,13 @@
+/**
+ * main - entry point
+ *
+ * Return: returns 1
+ */
+#include <stdio.h>
+int main(void)
+{
+	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fputs(s, stderr);
+	return (1);
+}
