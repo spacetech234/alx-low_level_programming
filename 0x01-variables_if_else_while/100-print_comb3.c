01e0c6630e9a6837f888c130b60d596495fdc6f5
Oluwatobi Adeoye <adeoyeoluwatobi90@gmail.com>
2023-03-20 20:06:23 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - starting point
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			if (i != j)
+			{
+				putchar(i % 10 + '0');
+				putchar(j % 10 + '0');
+
+				if (i == 8)
+				{
+					if (j == i + 1)
+					{
+						continue;
+					}
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
